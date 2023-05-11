ff54e3f3be5811d727dcdcda2139385dffe84444
tiago <tiagofiliperangel@gmail.com>
2023-05-11 10:33:43 +0100

    [IMP] sale_operating_unit: pre-commit stuff

--- sale_operating_unit/wizard/sale_make_invoice_advance.py
+++ sale_operating_unit/wizard/sale_make_invoice_advance.py
@@ -6,6 +6,6 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
     def _create_invoices(self, sale_orders):
         res = super(SaleAdvancePaymentInv, self)._create_invoices(sale_orders)
-        if self.advance_payment_method != 'delivered':
+        if self.advance_payment_method != "delivered":
             res.operating_unit_id = sale_orders.operating_unit_id.id
         return res
